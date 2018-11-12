f165000a94d5a1184fccfec3badce42e9ec8f88d
unknown <lopezmorenodana614@hotmail.com>
2018-11-12 13:36:44 -0600

    Se agrego el programa de cambio de moneda

---
+++ Frank/cambio de moneda.py
@@ -0,0 +1,7 @@
+def tipodecambio(dolares):
+    return dolares*20.3179
+def tipodecambiopesos(pesos):
+    return pesos /20.3179
+print('USD 20,000 es igual a: '+str(tipodecambio(20000))+'Pesos')
+print('$ 20,000 es igual a: '+str(tipodecambiopesos(20000))+'USD')
+
