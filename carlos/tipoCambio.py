66ef62334c8332a8c8ed2d3ad78c2ada5be28158
Carlos Solis Madrigal <ca_solis@hotmail.com>
2018-11-12 14:09:56 -0600

    Se refactorizó programa para calculo de tipo de cambio

--- carlos/tipoCambio.py
+++ carlos/tipoCambio.py
@@ -1,9 +1,24 @@
-def tipoCambio(dolares):
-    return dolares * 20.3179
+def tipoCambio(origen,destino,cantidad):
+    usdApesos = 20.3179
+    usdAEur = 0.89
+    if origen == "MXN":
+        if destino == "USD":
+            return cantidad / usdApesos
+        else:
+            return tipoCambio("USD","EUR",tipoCambio("MXN","USD",cantidad))
+    elif origen == "USD":
+        if destino == "MXN":
+            return cantidad * usdApesos
+        else:
+            return cantidad * usdAEur
+    else:
+        if destino == "MXN":
+            return tipoCambio("USD","MXN",tipoCambio("EUR","USD",cantidad))
+        else:
+            return cantidad / usdAEur
 
-def tipoCambioPesos(pesos):
-    return pesos / 20.3179
-
-print("USD 20,000 es igual a: " + str(tipoCambio(20000))+ " pesos")
-print("$ 20,000 pesos es igual a: " + str(tipoCambioPesos(20000))+ " USD")
+print("USD 20,000 es igual a: " + str(tipoCambio("USD","MXN",20000))+ " pesos")
+print("$ 20,000 pesos es igual a: " + str(tipoCambio("MXN","USD",20000))+ " USD")
+print("$ 20,000 pesos es igual a: " + str(tipoCambio("MXN","EUR",20000))+ " EUR")
+print("$ 20,000 EUR es igual a: " + str(tipoCambio("EUR","MXN",20000))+ " Pesos")
 
