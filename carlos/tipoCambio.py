3359df0aeab31a15390fe709dd05eb6a34825d7d
Carlos Solis Madrigal <ca_solis@hotmail.com>
2018-11-15 07:34:44 -0600

    Se impolemento cambio a yenes

--- carlos/tipoCambio.py
+++ carlos/tipoCambio.py
@@ -1,24 +1,31 @@
 def tipoCambio(origen,destino,cantidad):
     usdApesos = 20.3179
     usdAEur = 0.89
-    if origen == "MXN":
-        if destino == "USD":
-            return cantidad / usdApesos
-        else:
-            return tipoCambio("USD","EUR",tipoCambio("MXN","USD",cantidad))
-    elif origen == "USD":
+    usdAYen = 113.47
+    if origen == "USD":
         if destino == "MXN":
             return cantidad * usdApesos
-        else:
+        elif destino == "EUR":
             return cantidad * usdAEur
-    else:
-        if destino == "MXN":
-            return tipoCambio("USD","MXN",tipoCambio("EUR","USD",cantidad))
+        elif destino == "JPY":
+            return cantidad * usdAYen
         else:
+            return cantidad
+    if destino == "USD":
+        if origen == "MXN":
+            return cantidad / usdApesos
+        elif origen == "EUR":
             return cantidad / usdAEur
+        elif origen == "JPY":
+            return cantidad / usdAYen
+        else:
+            return cantidad
+    else:
+        return tipoCambio("USD",destino,tipoCambio(origen,"USD",cantidad))
 
 print("USD 20,000 es igual a: " + str(tipoCambio("USD","MXN",20000))+ " pesos")
 print("$ 20,000 pesos es igual a: " + str(tipoCambio("MXN","USD",20000))+ " USD")
 print("$ 20,000 pesos es igual a: " + str(tipoCambio("MXN","EUR",20000))+ " EUR")
-print("$ 20,000 EUR es igual a: " + str(tipoCambio("EUR","MXN",20000))+ " Pesos")
+print("20,000 EUR es igual a: " + str(tipoCambio("EUR","MXN",20000))+ " Pesos")
+print("20,000 JPY es igual a: " + str(tipoCambio("JPY","MXN",20000))+ " Pesos")
 
